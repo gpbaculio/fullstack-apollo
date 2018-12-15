2c794f67f0335b7d3b4cbbc14107d50d4d9fa39f
Glendon Philipp Baculio <gpbaculio@gmail.com>
2018-12-15 11:24:15 -0500

    setup add todo

--- frontend/src/components/Forms/AddTodo.js
+++ frontend/src/components/Forms/AddTodo.js
@@ -1,9 +1,56 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { Form, Input } from 'reactstrap'
+import PropTypes from 'prop-types'
 
-const AddTodo = () => (
-  <div>
-    AddTodo!
-  </div>
-)
+class AddTodo extends Component {
+
+  state = {
+    text: '',
+  }
+
+  onChange = e => {
+    const { name, value } = e.target
+    this.setState({
+      [name]: value
+    });
+  }
+
+  onSubmit = async (e) => {
+    e.preventDefault()
+    console.log('add submit')
+  };
+
+  render() {
+    const { text } = this.state
+    const { loading } = this.props
+    return (
+      <Form
+        className="
+          mx-auto
+          mt-4
+          mb-xs-1
+          mb-md-5
+          align-items-start
+        "
+        onSubmit={this.onSubmit}
+      >
+        <Input
+          type="text"
+          id="text"
+          name="text"
+          value={text}
+          placeholder="Add Todo"
+          disabled={loading}
+          onChange={this.onChange}
+          className="form-control w-75"
+        />
+      </Form>
+    )
+  }
+}
+
+AddTodo.propTypes = {
+  loading: PropTypes.bool.isRequired,
+}
 
 export default AddTodo
