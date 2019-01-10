4ef962d6c3e5a926f3255853b2d962b3775a069d
Glendon Philipp Baculio <gpbaculio@gmail.com>
2019-01-09 21:14:58 -0500

    add graphql on forntend

---
+++ frontend/build/precache-manifest.58ff6dd77c43f19bb3ced519d375a259.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "7ce82537d40c3e1593f5",
+    "url": "/static/js/main.7ce82537.chunk.js"
+  },
+  {
+    "revision": "02de0e936ee7cd766761",
+    "url": "/static/js/1.02de0e93.chunk.js"
+  },
+  {
+    "revision": "7ce82537d40c3e1593f5",
+    "url": "/static/css/main.64d668e8.chunk.css"
+  },
+  {
+    "revision": "02de0e936ee7cd766761",
+    "url": "/static/css/1.64cefe84.chunk.css"
+  },
+  {
+    "revision": "655ce8c0d793a5a72d2c1f878afaaf69",
+    "url": "/index.html"
+  }
+];
