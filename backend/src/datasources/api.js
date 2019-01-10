518a0eb794c8fdb2794dcc21400c17b8392b139c
Glendon Philipp Baculio <gpbaculio@gmail.com>
2019-01-09 23:18:17 -0500

    change datasource host

--- backend/src/datasources/api.js
+++ backend/src/datasources/api.js
@@ -1,71 +1,79 @@
 import { RESTDataSource } from 'apollo-datasource-rest';
-import map from 'lodash/map'
+import map from 'lodash/map';
 
 class Api extends RESTDataSource {
   constructor() {
     super();
-    this.baseURL = `${process.env.HOSTNAME || process.env.LOCALHOST}api`;
+    this.baseURL = `http://fullstack-apollo-graphql.herokuapp.com/api`;
   }
 
   async logIn({ email, password }) {
     const { token, error } = await this.post('/auth', { email, password });
-    return ({
+    return {
       token,
       error
-    });
+    };
   }
 
   async signUp({ email, password }) {
-    const response = await this.post('/user', { email, password })
-    return ({
+    const response = await this.post('/user', { email, password });
+    return {
       error: response.error,
       email: response.email
-    })
+    };
   }
 
   async addTodo({ text, userId }) {
-    const { todo } = await this.post('/todo', { text, userId })
-    return ({
+    const { todo } = await this.post('/todo', { text, userId });
+    return {
       todo
-    })
+    };
   }
 
-  async fetchTodos({ user: { id }, query: { offset, limit, complete, search } }) {
-    const { todos, count } = await this.get('/todo/fetchTodos', { id, offset, limit, complete, search })
-    return ({
+  async fetchTodos({
+    user: { id },
+    query: { offset, limit, complete, search }
+  }) {
+    const { todos, count } = await this.get('/todo/fetchTodos', {
+      id,
+      offset,
+      limit,
+      complete,
+      search
+    });
+    return {
       count,
       todos
-    })
+    };
   }
 
   async updateTodoText({ input, userId }) {
-    const { todo } = await this.post('/todo/updateText', { input, userId })
-    return ({
+    const { todo } = await this.post('/todo/updateText', { input, userId });
+    return {
       todo
-    })
+    };
   }
 
   async deleteTodo({ input, user }) {
-    const { _id } = await this.post('/todo/deleteTodo', { input, user })
-    return ({
+    const { _id } = await this.post('/todo/deleteTodo', { input, user });
+    return {
       _id
-    })
+    };
   }
 
   async toggleComplete({ input, user }) {
-    const { _ids } = await this.post('/todo/toggleComplete', { input, user })
-    return ({
+    const { _ids } = await this.post('/todo/toggleComplete', { input, user });
+    return {
       _ids: map(_ids, ({ _id }) => _id)
-    })
+    };
   }
 
   async clearCompleted({ input, user }) {
-    const { _ids } = await this.post('/todo/clearCompleted', { input, user })
-    return ({
+    const { _ids } = await this.post('/todo/clearCompleted', { input, user });
+    return {
       _ids
-    })
+    };
   }
-
 }
 
-export default Api
+export default Api;
